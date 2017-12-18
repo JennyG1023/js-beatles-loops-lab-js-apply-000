859c01db0c5e64a7cbd45cea00aac66190f74292
JennyG1023 <jennygao1023@gmail.com>
2017-12-18 21:42:27 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,27 @@
+function theBeatlesPlay(musicians, instruments) {
+  var facts = [];
+  for(var i = 0; i < musicians.length; i++) {
+    facts.push(musicians[i] + " plays "  + instruments[i].toLowerCase());
+  }
+  return facts;
+}
+
+function johnLennonFacts(facts) {
+  var facts_johnLennon = [];
+  var index = 0;
+  while (index < facts.length) {
+    facts_johnLennon.push(facts[index] + "!!!");
+    index++;
+  }
+  return facts_johnLennon;
+}
+
+function iLoveTheBeatles(num) {
+  var empty = [];
+  do {
+    empty.push("I love the Beatles!");
+    num+num;
+  }
+  while (num < 15);
+  return empty;
+}
